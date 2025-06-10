e977db23876ac001de60a0c11a1d511a134f1f71
matchaboisluv <ipongpsalmantha@gmail.com>
2025-06-10 18:51:43 +0800

    feat: add cooper hewitt font and configure in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        CooperHewitt: ['CooperHewitt', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
