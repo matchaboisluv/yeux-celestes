ae081cca8a28e759f029bbc8fa59a670fafa6e2b
matchaboisluv <ipongpsalmantha@gmail.com>
2025-06-10 19:01:37 +0800

    feat: add landing page with banner image and footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer(){
+    return(
+        <>
+        <div className="bg-white text-black py-1 pl-4 w-full font-CooperHewitt text-xs">
+            © rosie. All Rights Reserved.
+        </div>
+        </>
+    )
+}
+
+export default Footer
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,21 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import yeuxcelestes from '../assets/images/yeuxcelestes.png'
+
 function LandingPage(){
     return (
-        <>
-        <p>landing</p>
-        </>
+        <div className="flex flex-col min-h-screen w-full">
+            <main className="flex-grow flex flex-col items-center justify-center relative">
+                <div className="relative max-w-full md:max-w-[65%] lg:max-w-[46%] w-full">
+                    <img src={yeuxcelestes} className="w-full h-auto object-contain z-0"/>
+                    <button className="absolute top-[52%] left-1/2 transform -translate-x-1/2 px-4 py-1 border-2 border-white rounded-3xl hover:bg-white hover:text-[#640000] font-CooperHewitt text-[0.6rem] md:text-sm lg:text-md">
+                        Get Started
+                    </button>
+                </div>
+                
+            </main>
+            <Footer/>
+        </div>
     )
 }
 
