fb2a0f1042b51f8446c27a5c377ee66c2db7f89f
matchaboisluv <ipongpsalmantha@gmail.com>
2025-06-14 21:21:45 +0800

    style: add bottom-0 positioning class to footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer(){
     return(
         <>
-        <div className="bg-white text-black py-1 pl-4 w-full font-CooperHewitt text-xs">
+        <div className="bg-white text-black py-1 pl-4 w-full font-CooperHewitt text-xs bottom-0">
             © rosie. All Rights Reserved.
         </div>
         </>
