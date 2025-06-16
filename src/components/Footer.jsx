75b3fb8f2784e8358c10b574b7b639af49874f21
matchaboisluv <ipongpsalmantha@gmail.com>
2025-06-16 20:03:03 +0800

    fix: change div to semantic footer element

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
 function Footer(){
-    return(
-        <>
-        <div className="bg-white text-black py-1 pl-4 w-full font-CooperHewitt text-xs bottom-0">
+    return (
+        <footer className="bg-white text-black py-1 pl-4 w-full font-CooperHewitt text-xs">
             © rosie. All Rights Reserved.
-        </div>
-        </>
+        </footer>
     )
 }
 
