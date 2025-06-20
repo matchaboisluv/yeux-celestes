6279b001c23b64552d6a8dbc2062f4fef1af4237
matchaboisluv <ipongpsalmantha@gmail.com>
2025-06-20 18:24:53 +0800

    style: adjust landing page button opacity and font size for consistency

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -17,7 +17,7 @@ function LandingPage(){
                     <img src={yeuxcelestes} className="w-full h-auto object-contain z-0 animate-pulse brightness-150 contrast-105"/>
                     <button
                         onClick={handleGetStarted} 
-                        className="absolute top-[52%] left-1/2 transform -translate-x-1/2 px-4 md:px-6 md:py-2.5 border-[1px] md:border-[1px] lg:border-2 xl:border-2 border-white rounded-full font-CooperHewitt text-[0.6rem] md:text-sm lg:text-base font-medium text-white transition-all duration-1000 hover:bg-white hover:text-red-800 active:scale-95 hover:shadow-[0_0_10px_rgba(255,255,0,0.5)]"
+                        className="opacity-[90%] absolute top-[52%] left-1/2 transform -translate-x-1/2 px-4 md:px-6 py-1 md:py-2.5 border-[1px] md:border-[1px] lg:border-2 xl:border-2 border-white rounded-full font-CooperHewitt text-[0.6rem] md:text-sm lg:text-sm font-medium text-white transition-all duration-1000 hover:bg-white hover:text-red-800 active:scale-95 hover:shadow-[0_0_10px_rgba(255,255,0,0.5)]"
                     >
                         Begin Experience
                     </button>
